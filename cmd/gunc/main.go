81f9eccabce6bf9182352a1d25638122bdfbb679
0xbatutut <39509105+0xbatutut@users.noreply.github.com>
2018-08-21 14:00:00 +0700

    Change help's texts and default dir of chain

--- cmd/gunc/main.go
+++ cmd/gunc/main.go
@@ -167,7 +167,7 @@ func init() {
 	// Initialize the CLI app and start Geth
 	app.Action = geth
 	app.HideVersion = true // we have a command to print the version
-	app.Copyright = "Copyright 2013-2018 The go-ethereum Authors"
+	app.Copyright = "Copyright 2013-2018 The go-ethereum and go-uncommons Authors"
 	app.Commands = []cli.Command{
 		// See chaincmd.go:
 		initCommand,
